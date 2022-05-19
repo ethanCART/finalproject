6a11f30f6a9842c7dd189324af86fab5794d9c1c
ethanCART <94005185+ethanCART@users.noreply.github.com>
2022-05-19 09:32:18 -0700

    k

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+window.onload = function(){
+var insults ={
+    test1: {
+        text: 'test'
+    }
+}
+
 var generatorBox = document.getElementById("generatorBox");
+
+function addStory(text){
+    generatorBox.innerHTML = text;
+
+  }
+  function createStory(text){
+    Math.floor(Math.random() * 100);
+    let pageNow = playerChoices[playerChoices.length - 1];
+    generatorBox.innerHTML = "";
+    updateImage(story[pageNow].imgElem);
+    for(let idea of playerChoices){
+      addStory(story[idea].text)
+    }
+    for (let idea of story[pageNow].options){
+      createButton(idea[1],idea[0]);
+    }
+  
+  }
+  restart.addEventListener("click", function(){
+    location.reload();
+  });
+  
+  restart.style.display = 'none';
+  
+  x.addEventListener("click", function(){
+    createStory(story.opener.text);
+    restart.style.display = '';
+  });
+  
+}
