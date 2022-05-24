d1a1546f22e22cf8026216b65ba5bf629e73d84c
ethanCART <94005185+ethanCART@users.noreply.github.com>
2022-05-24 09:31:31 -0700

    a

--- script.js
+++ script.js
@@ -39,4 +39,37 @@ function addStory(text){
     restart.style.display = '';
   });
   
+  class CommentList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+      this.state = {
+        comments: DataSource.getComments()
+      };
+    }
+  
+    componentDidMount() {
+      DataSource.addChangeListener(this.handleChange);
+    }
+  
+    componentWillUnmount() {
+      DataSource.removeChangeListener(this.handleChange);
+    }
+  
+    handleChange() {
+      this.setState({
+        comments: DataSource.getComments()
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          {this.state.comments.map((comment) => (
+            <Comment comment={comment} key={comment.id} />
+          ))}
+        </div>
+      );
+    }
+  }
 }
