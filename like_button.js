dc00dfadd55a33c2ccd39deb416e432db9c8c056
ethanCART <94005185+ethanCART@users.noreply.github.com>
2022-05-25 10:24:38 -0700

    n

--- like_button.js
+++ like_button.js
@@ -20,13 +20,19 @@ class LikeButton extends React.Component {
     );
   }
 }
-
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
-
-const root = ReactDOM.createRoot(
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+  
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
   );
-  const element = <h1>Hello, world</h1>;
   root.render(element);
+}
+
+setInterval(tick, 1000);
+
+
+// const root = reactDOM.
